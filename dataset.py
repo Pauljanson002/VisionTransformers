08b1a8b4ed2bdc446657123ce3e5144b9fe29b61
Paul Janson <pauljanson002@gmail.com>
2021-06-19 16:03:07 +0530

    Changed download to true

--- dataset.py
+++ dataset.py
@@ -2,7 +2,7 @@ from torchvision import datasets, transforms
 
 data_path = './data'
 cifar10 = datasets.CIFAR10(
-        data_path, train=True, download=False,
+        data_path, train=True, download=True,
         transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize(
@@ -12,7 +12,7 @@ cifar10 = datasets.CIFAR10(
         ])
     )
 cifar10_val = datasets.CIFAR10(
-    data_path, train=False, download=False,
+    data_path, train=False, download=True,
     transform=transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(
