8781895fc313b8bebfada45321982d95428c2377
Paul Janson <pauljanson002@gmail.com>
2021-08-01 10:09:26 +0530

    Added best practices and wandb support

--- models/model_factory.py
+++ models/model_factory.py
@@ -9,84 +9,90 @@ from models.ViTLiteSeq import ViTLiteSeq
 
 
 def create_model(model_name: str):
-    models = ['vit', 'vit_naive', 'vit_lite', 'vgg', 'deit', 'cct', 'vit_lite_2', 'deit_lite', 'vit_lite_h',
-              'vit_lite_seq','vit_lite_100','cct7']
-    if model_name not in models:
-        raise NotImplementedError("The model you asked is not implemented")
+    if model_name == 'vit_lite':
+        return ViTLite(
+            num_layers=7,
+            num_heads=4,
+            mlp_ratio=2,
+            embedding_dim=256,
+            patch_size=4,
+        )
+    elif model_name == 'vit_lite_100':
+        return ViTLite(
+            num_layers = 14,
+            num_heads=8,
+            mlp_ratio= 2,
+            embedding_dim=512,
+            patch_size=4,
+            num_classes = 100
+        )
+    elif model_name == 'vit_lite_seq':
+        return ViTLiteSeq(
+            num_layers=14,
+            num_heads=4,
+            mlp_ratio=2,
+            embedding_dim=256,
+            patch_size=4
+        )
+    elif model_name == 'vit_lite_2':
+        return ViTLite(
+            num_layers=14,
+            num_heads=4,
+            mlp_ratio=2,
+            embedding_dim=256,
+            patch_size=4
+        )
+    elif model_name == 'vit_lite_h':
+        return ViTLite(
+            num_layers=32,
+            num_heads=4,
+            mlp_ratio=2,
+            embedding_dim=256,
+            patch_size=4
+        )
+    elif model_name == 'vit_naive':
+        return ViTNaive()
+    elif model_name == 'vgg':
+        return VGG('VGG16')
+    elif model_name == 'deit':
+        return Deit(emb_size=108)
+    elif model_name == 'cct':
+        return CCT(num_layers=14,
+                   num_heads=4,
+                   mlp_ratio=2,
+                   embedding_dim=256,
+                   kernel_size=3,
+                   stride=1,
+                   padding=1)
+    elif model_name == 'cct7':
+        return CCT(
+            num_layers= 6,
+            num_heads=4,
+            mlp_ratio=2,
+            embedding_dim=256,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            num_classes = 100
+        )
+    elif model_name == 'cct7_h':
+        return CCT(
+            num_layers=6,
+            num_heads=4,
+            mlp_ratio=2,
+            embedding_dim=512,
+            kernel_size=3,
+            stride=1,
+            padding=1,
+            num_classes=100
+        )
+    elif model_name == 'deit_lite':
+        return DeiTLite(
+            num_layers=14,
+            num_heads=4,
+            mlp_ratio=2,
+            embedding_dim=256,
+            patch_size=4
+        )
     else:
-        if model_name == 'vit_lite':
-            return ViTLite(
-                num_layers=7,
-                num_heads=4,
-                mlp_ratio=2,
-                embedding_dim=256,
-                patch_size=4,
-            )
-        elif model_name == 'vit_lite_100':
-            return ViTLite(
-                num_layers = 14,
-                num_heads=8,
-                mlp_ratio= 2,
-                embedding_dim=512,
-                patch_size=4,
-                num_classes = 100
-            )
-        elif model_name == 'vit_lite_seq':
-            return ViTLiteSeq(
-                num_layers=14,
-                num_heads=4,
-                mlp_ratio=2,
-                embedding_dim=256,
-                patch_size=4
-            )
-        elif model_name == 'vit_lite_2':
-            return ViTLite(
-                num_layers=14,
-                num_heads=4,
-                mlp_ratio=2,
-                embedding_dim=256,
-                patch_size=4
-            )
-        elif model_name == 'vit_lite_h':
-            return ViTLite(
-                num_layers=32,
-                num_heads=4,
-                mlp_ratio=2,
-                embedding_dim=256,
-                patch_size=4
-            )
-        elif model_name == 'vit_naive':
-            return ViTNaive()
-        elif model_name == 'vgg':
-            return VGG('VGG16')
-        elif model_name == 'deit':
-            return Deit(emb_size=108)
-        elif model_name == 'cct':
-            return CCT(num_layers=14,
-                       num_heads=4,
-                       mlp_ratio=2,
-                       embedding_dim=256,
-                       kernel_size=3,
-                       stride=1,
-                       padding=1)
-        elif model_name == 'cct7':
-            return CCT(
-                num_layers= 6,
-                num_heads=4,
-                mlp_ratio=2,
-                embedding_dim=256,
-                kernel_size=3,
-                stride=1,
-                padding=1,
-                num_classes = 100
-            )
-        elif model_name == 'deit_lite':
-            return DeiTLite(
-                num_layers=14,
-                num_heads=4,
-                mlp_ratio=2,
-                embedding_dim=256,
-                patch_size=4
-            )
-        else:
-            return ViT()
+        raise NotImplementedError("Model is not implemented")
--- run.ps1
+++ run.ps1
@@ -1,4 +1,5 @@
 #Vit lite with 10 heads 7 layers 512 embeding dimension
 #python -u train_v2.py --savename vit_lite_100_7L.pt --epoch 100 --batch_size 128 --learning_rate 0.0005 --weight_decay 3e-2 --model vit_lite_100 --dataset cifar100 --online --run_name vit_on_cifar100_14layer8head
 
+#CCT with 4 heads and 6 layers 256 embeding dimension
 python -u train_v2.py --savename vit_lite_100_cct.pt --epoch 10 --batch_size 64 --learning_rate 0.0005 --weight_decay 3e-2 --model cct7 --dataset cifar100  --run_name cct_on_cifar100
